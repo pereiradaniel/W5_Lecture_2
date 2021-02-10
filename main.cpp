eb2fe39b518962828d35d11a447dc4e96ebaf0fe
pereiradaniel <danielpaulpereira@protonmail.com>
2021-02-10 13:21:38 -0500

    Complete

--- main.cpp
+++ main.cpp
@@ -9,6 +9,21 @@ void display(int* arr, size_t size)
 	cout << endl;
 }
 
+enum class Order {ASC, DESC};
+class Comparator	// Functor: Function class!
+{
+public:
+	Order m_order = Order::ASC;
+	bool operator()(int a, int b)
+	{
+		cout << "operator()\n";
+		if (m_order == Order::ASC)
+			return a > b; // Return a boolean value!
+		else
+			return a < b;
+	}
+};
+
 bool asc(int a, int b)
 {
 	// Should the values be switched?
@@ -20,7 +35,8 @@ bool desc(int a, int b)
 	return a < b;
 }
 
-void bubbleSort(int* arr, size_t n, bool (*cmp)(int, int))
+template<typename T> 
+void bubbleSort(int* arr, size_t n, T cmp)
 {
 	for (auto i = 0u; i < n - 1; ++i)
 		for (auto j = i + 1; j < n; ++j)
@@ -35,6 +51,10 @@ void bubbleSort(int* arr, size_t n, bool (*cmp)(int, int))
 
 int main()
 {
+	Comparator cmpFO;	// This instance can be used like a function!
+
+	// cout << "Should I switch? " << cmpFO(12, 9) << endl;
+
 	bool (*cmpF)(int, int) = nullptr; // pointer to a function that returns bool.
 
 	char choice = '\0';
@@ -42,9 +62,15 @@ int main()
 	cin >> choice;
 
 	if (choice == 'a')
+	{
+		cmpFO.m_order = Order::ASC;
 		cmpF = asc;
+	}
 	else
+	{
+		cmpFO.m_order = Order::DESC;
 		cmpF = desc;
+	}
 
 	int arr[]{ 4, 6, 98, 1, -5, 5, 78 };
 	display(arr, sizeof (arr) / sizeof (arr[0]));
